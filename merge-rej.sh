d5e8be360dff61b6ae581e56d9ff4e1b6676edb6
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2016-05-13 10:54:30 +0100

    Open in official intellij and just attempt to merge reject files

--- merge-rej.sh
+++ merge-rej.sh
@@ -2,7 +2,7 @@
 
 # This just attempts to merged rejected files.
 
-git ls-files --others --exclude-standard | while read reject
+git ls-files --others --exclude-standard \*.rej | while read reject
 do
   file=$(echo ${reject} | sed 's/\.rej$//')
   echo Merging in $file
@@ -10,7 +10,8 @@ do
   wiggle -r -v $file $reject
   echo Hit enter to open in intellij
   read -s -n 1 key < /dev/tty
-  open -b com.jetbrains.intellij.ce $file
+  #open -b com.jetbrains.intellij.ce $file
+  open -b com.jetbrains.intellij $file
   echo Hit enter when we can remove reject file: 
   read -s -n 1 key < /dev/tty
   rm $reject
